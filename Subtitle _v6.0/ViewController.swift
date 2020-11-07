6ef436c165c1eacf195d1b502c983a768ac645d5
lgl <lk23286@gmail.com>
2020-11-07 11:28:52 +0100

    Initial Commit

---
+++ Subtitle _v6.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subtitle _v6.0
+//
+//  Created by Laszlo Kovacs on 2020. 11. 07..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
